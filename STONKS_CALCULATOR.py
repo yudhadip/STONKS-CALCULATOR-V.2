478600abf6d81a41b1039ada609cf8a34d503d1b
yudhadip <76836682+yudhadip@users.noreply.github.com>
2021-01-12 20:20:41 +0700

    Update STONKS_CALCULATOR.py

--- STONKS_CALCULATOR.py
+++ STONKS_CALCULATOR.py
@@ -1,1 +1,18 @@
-print("hello world")
+import numpy
+print("STONKS CALCULATOR")
+x = float(input("masukan modal: "))
+b1 = float(input("masukan jumlah persen yang diinginkan (%): "))
+b2 = 100
+z = x * (b1 / b2)
+total = 0
+limit_min = 1
+limit_max = float(input("masukan jumlah hari yang diinginkan: "))
+step = 1
+a = 1
+
+for total in numpy.arange(limit_min, limit_max, step):
+    total += (x + z) - a
+    x = total
+    z = x * (b1 / b2)
+    a += 1
+    print(total)
