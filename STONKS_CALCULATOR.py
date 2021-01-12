6c019a16226de12c4a4f6f2e3f51f3a9e2304c3a
yudhadip <76836682+yudhadip@users.noreply.github.com>
2021-01-12 22:58:31 +0700

    Update STONKS_CALCULATOR.py

--- STONKS_CALCULATOR.py
+++ STONKS_CALCULATOR.py
@@ -1,17 +1,44 @@
-import numpy
+from numpy import arange
+import sys
+import re
 
 print("STONKS CALCULATOR")
-x = float(input("masukan modal: "))
-b1 = float(input("masukan jumlah persen yang diinginkan (%): "))
+
+x = (input("masukan modal: "))
+
+try:
+    float(x)
+except ValueError:
+    print("yang dimasukin angka tolol bukan lu?")
+    sys.exit()
+
+b1 = (input("masukan jumlah persen yang diinginkan (%): "))
+
+try:
+    float(b1)
+except ValueError:
+    print("yang dimasukin angka tolol bukan lu?")
+    sys.exit()
+
+limit_max = (input("masukan jumlah hari yang diinginkan: "))
+
+try:
+    float(limit_max)
+except ValueError:
+    print("yang dimasukin angka tolol bukan lu?")
+    sys.exit()
+
+x = float(x)
+b1 = float(b1)
+limit_max = float(limit_max)
 b2 = 100
-z = x * (b1 / b2)
 total = 0
 limit_min = 1
-limit_max = float(input("masukan jumlah hari yang diinginkan: "))
+z = x * (b1 / b2)
 step = 1
 a = 1
 
-for total in numpy.arange(limit_min, limit_max, step):
+for total in arange(limit_min, limit_max, step):
     total += (x + z) - a
     x = total
     z = x * (b1 / b2)
